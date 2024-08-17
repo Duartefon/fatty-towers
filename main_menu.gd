f410b030b8cfd4c13058ee212fe1ec7f14f40a14
Sara Marques <saramarquesgamedev@gmail.com>
2024-08-17 16:08:13 +0100

    Added placeholder options menu and menu switching

--- main_menu.gd
+++ main_menu.gd
@@ -1,13 +1,12 @@
 extends Control
 
-
-
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://playable_scene.tscn")
 
 
 func _on_options_button_pressed() -> void:
-	pass # Replace with function body.
+	$MarginContainer.visible = false
+	$OptionsMenu.visible = true
 
 
 func _on_tutorial_button_pressed() -> void:
@@ -20,3 +19,8 @@ func _on_story_button_pressed() -> void:
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_back_to_menu_pressed() -> void:
+	$OptionsMenu.visible = false
+	$MarginContainer.visible = true
