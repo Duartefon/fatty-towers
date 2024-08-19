9a363ec2d5873b6f802b8345f58db6457745f9fb
Duarte <deck@steamdeck.nonius.hsia>
2024-08-19 13:18:32 +0100

    most of the scale done

--- scene_manager.gd
+++ scene_manager.gd
@@ -1,9 +1,12 @@
 extends Path2D
 @onready var block_sprite: Sprite2D = $PendulumFollow/BlockSprite
 @export var my_array: Array[PackedScene] = []  
-@onready var scale_plate_left: StaticBody2D = $"../ScalePlate_left"
-@onready var scale_plate_right: StaticBody2D = $"../ScalePlate_right"
+@onready var scale_plate_left: StaticBody2D = $"../ScalePlateLeft"
+@onready var scale_plate_right: StaticBody2D = $"../ScalePlateRight"
+
 @export var MAX_WEIGHT = 500 
+var total_weight_left = 0  # estas 3 var nao devem ser preciso 
+var total_weight_right = 0
 #os pratos tao hardcoded cuidado ao dar rename na scene tree
 var current_block 
 #array de blocos a partir do inspetor ou assim - done
@@ -23,29 +26,32 @@ func select_block():
 	block_sprite.texture = new_block.get_node("RigidBody2D/Sprite2D").texture
 	block_sprite.scale = new_block.get_node("RigidBody2D/Sprite2D").scale
 	return new_block	
+	
 func spawn():
 	var new_block = select_block()
 	current_block = new_block
 	new_block.global_position = block_sprite.global_position
 	add_child(new_block)
-
-	
 	new_block.connect("block_stopped", _on_block_stopped)
 	
-
-func _on_block_stopped(pos,weight):
+func _on_block_stopped(pos, weight):
 	print("Block has stopped moving!")
-	
-	if position.length_squared() - scale_plate_left.position.length_squared() < position.length_squared() - scale_plate_right.position.length_squared():  
+
+	var dist_to_left = current_block.global_position.distance_squared_to(scale_plate_left.global_position)
+	var dist_to_right = current_block.global_position.distance_squared_to(scale_plate_right.global_position)
+
+	if dist_to_left < dist_to_right: # proximo do prato esquerdo
 		current_block.reparent(scale_plate_left)
-		Global.score += weight	 
-		teste = scale_plate_left.global_position * weight / MAX_WEIGHT * 20
-		scale_plate_left.global_position *= weight / MAX_WEIGHT * 20 
-	elif position.length_squared() - scale_plate_left.position.length_squared() > position.length_squared() - scale_plate_right.position.length_squared(): 
+		scale_plate_left.position.y += weight   
+		scale_plate_right.position.y -= weight
+		Global.score += weight
+	elif dist_to_left > dist_to_right: #proximo prato direito
 		current_block.reparent(scale_plate_right)
-		Global.score += weight 
-	else: Global.score -= weight
-	
+		scale_plate_right.position.y += weight
+		scale_plate_left.position.y -= weight
+		Global.score += weight
+	else:
+		Global.score -= weight
 	
 	#função de spawn (quando se prime espaço) - dne
 	# spawn do block - done
